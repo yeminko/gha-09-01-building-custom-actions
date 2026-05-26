e474d2c992a115351dfe2bf7224637638717c115
yeminko <yeminko47@gmail.com>
2026-05-26 17:43:09 +0630

    feat: add website URL output to S3 deployment action and update workflow to display it [skip ci]

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -15,6 +15,9 @@ async function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   core.notice("Hello from my custom JavaScript action!");
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
