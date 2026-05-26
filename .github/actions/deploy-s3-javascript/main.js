5bc76f2e427d75de620b231467bc7f0d5b6b869e
yeminko <yeminko47@gmail.com>
2026-05-26 17:37:51 +0630

    feat: enhance S3 deployment action with input parameters and update workflow to use them [skip ci]

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 async function run() {
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", {
+    required: true,
+  });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // github.context; // Access GitHub context if needed
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("Hello from my custom JavaScript action!");
 }
 
